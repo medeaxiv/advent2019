aed67df56f3c39fd640ee09d9b9a79fa9063e29c
Medea <105075687+medeaxiv@users.noreply.github.com>
2024-01-22 11:38:35 -0500

    Add a min_max function to the numerics utilities

--- src/util/numerics.rs
+++ src/util/numerics.rs
@@ -22,3 +22,14 @@ where
     let gcd = greatest_common_divisor(a.clone(), b.clone());
     (a * b) / gcd
 }
+
+pub fn min_max<T>(a: T, b: T) -> (T, T)
+where
+    T: PartialOrd,
+{
+    if b < a {
+        (b, a)
+    } else {
+        (a, b)
+    }
+}
